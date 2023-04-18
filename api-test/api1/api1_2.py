0bfa5f329a6b08c70daf458e8d97235bbb030bd6
MANJUNATH SK <50445995+SK1793@users.noreply.github.com>
2023-04-18 12:36:30 +0530

    Update api1_2.py

--- api-test/api1/api1_2.py
+++ api-test/api1/api1_2.py
@@ -47,6 +47,7 @@ def predict():
     cgpa = request.form.get('cgpa')
     iq = request.form.get('iq')
     profile_score = request.form.get('profile_score')
+    cgpa,profile_score,iq=8.0,90,100
     input_query = np.array([[cgpa,iq,profile_score]])
     result = model.predict(input_query)[0]
     placement=jsonify({'placement':str(result)})
